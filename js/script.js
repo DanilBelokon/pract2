119d98a22a2e4f61c3e7862b1c4a6dc7de1d3204
Danil <ilya.belokon.07@gmail.com>
2025-02-16 18:30:18 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,49 @@
+/* Задания на урок:
+
+1) Удалить все рекламные блоки со страницы (правая часть сайта)
+
+2) Изменить жанр фильма, поменять "комедия" на "драма"
+
+3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
+Реализовать только при помощи JS
+
+4) Список фильмов на странице сформировать на основании данных из этого JS файла.
+Отсортировать их по алфавиту 
+
+5) Добавить нумерацию выведенных фильмов */
+
+"use strict";
+
+const movieDB = {
+  movies: [
+    "Логан",
+    "Лига справедливости",
+    "Ла-ла лэнд",
+    "Одержимость",
+    "Скотт Пилигрим против...",
+  ],
+};
+
+document.querySelectorAll(".promo__adv img").forEach((item) => {
+  item.remove();
+});
+
+document.querySelector(".promo__genre").textContent = "драма";
+// document.querySelector(".promo__bg").style.background =
+//   "url(../img/bg.jpg) center center / cover no-repeat";
+document.querySelector(".promo__bg").style.backgroundImage =
+  "url('img/bg.jpg')";
+movieDB.movies.sort();
+// document.querySelectorAll(".promo__interactive-item").forEach((item, ind) => {
+//   item.textContent = `${ind + 1}. ${movieDB.movies[ind]}`;
+//   console.log(ind);
+// });
+
+const movlist = document.querySelector(".promo__interactive-list");
+movlist.innerHTML = "";
+
+movieDB.movies.forEach((film, id) => {
+  movlist.innerHTML += `<li class="promo__interactive-item">${id + 1}. ${film}
+                            <div class="delete"></div>
+                        </li>`;
+});
